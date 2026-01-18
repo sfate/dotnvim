b98eebf1e5bc48c5198c7e82785fd4ff99d8dd11
Oleksii Bobyriev <alexey.bobyrev@gmail.com>
2026-01-18 23:59:51 +0100

    feat: add tf support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,6 +25,7 @@ require('lazy').setup({
   { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
   -- Language packs and syntax
   { 'tpope/vim-commentary', name = 'commentary' },
+  { "allaman/tf.nvim", opts = {}, ft = "terraform" },
   -- Navigation
   { 'ibhagwan/fzf-lua', dependencies = { 'kyazdani42/nvim-web-devicons' } },
   -- Colors
