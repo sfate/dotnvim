6d33c8752fc7073cfb8deb1ee1146f2a68a0e5f2
Oleksii Bobyriev <alexey.bobyrev@gmail.com>
2026-03-17 11:40:02 -0700

    fix: reset theme on system light appearance change

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,6 +8,14 @@ function set_theme_from_system()
   local result = handle:read("*a")
   handle:close()
 
+  -- Clear cached module failures so nvim-web-devicons can reload cleanly
+  package.loaded['nvim-web-devicons.icons-light'] = nil
+  package.loaded['nvim-web-devicons.light.icons_by_filename'] = nil
+  package.loaded['nvim-web-devicons.light.icons_by_file_extension'] = nil
+  package.loaded['nvim-web-devicons.light.icons_by_operating_system'] = nil
+  package.loaded['nvim-web-devicons.light.icons_by_desktop_environment'] = nil
+  package.loaded['nvim-web-devicons.light.icons_by_window_manager'] = nil
+
   if result:match("Dark") then
     vim.o.background = "dark"
   else
