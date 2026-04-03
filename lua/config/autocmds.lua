a5074c4c6109f34b2451399905daab19eb6a8e6a
Oleksii Bobyriev <alexey.bobyrev@gmail.com>
2026-04-03 22:54:11 +0200

    no args - netrw + parse file:line

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -24,3 +24,23 @@ api.nvim_create_autocmd('FileType', {
   pattern = 'netrw',
   command = 'setlocal bufhidden=wipe',
 })
+
+-- Open netrw on no args; handle file:line arguments (e.g. nvim file.go:378)
+api.nvim_create_autocmd('VimEnter', {
+  nested = true,
+  callback = function()
+    if vim.fn.argc() == 0 then
+      vim.cmd('Explore')
+      return
+    end
+    local arg = vim.fn.argv(0)
+    local file, lnum = arg:match('^(.+):(%d+)$')
+    if file and lnum and vim.fn.filereadable(file) == 1 then
+      local fake_buf = api.nvim_get_current_buf()
+      vim.cmd('edit +' .. lnum .. ' ' .. vim.fn.fnameescape(file))
+      if api.nvim_buf_is_valid(fake_buf) then
+        api.nvim_buf_delete(fake_buf, { force = true })
+      end
+    end
+  end,
+})
